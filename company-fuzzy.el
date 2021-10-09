1f66534c8d34170f58fcc28b31ebef758e727344
Jen-Chieh Shen <jcs090218@gmail.com>
2021-10-09 19:58:07 +0800

    Adapt all data

--- company-fuzzy.el
+++ company-fuzzy.el
@@ -593,7 +593,7 @@ Insert .* between each char."
     (annotation (company-fuzzy--extract-annotation arg))
     (candidates (company-fuzzy-all-candidates))
     (pre-render (company-fuzzy--pre-render arg (nth 0 ignored)))
-    ((or doc-buffer kind) (company-fuzzy--backend-command arg command))))
+    (t (company-fuzzy--backend-command arg command))))
 
 (provide 'company-fuzzy)
 ;;; company-fuzzy.el ends here
